0b741595194a7e7db96a33c21352059bde19f9d4
QuangTTb <QuangTTb@bkav.com>
2023-01-30 11:03:49 +0700

    Fixed feature-1 successfully

--- feature-1/Feature1.java
+++ feature-1/Feature1.java
@@ -2,5 +2,6 @@ class Feature1 {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is for testing with feature-1");
+        System.out.println("This line added to test hotfix feature!");
     }
 }
